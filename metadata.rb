be6877a19172fbf8c72a18f95fc05024a8c72843
Atalanta Systems Engineering <atalanta@drax.atalanta-systems.com>
2011-10-14 17:22:07 +0100

    Correcting README file location

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Atalanta Systems Ltd"
 maintainer_email "support@atalanta-systems.com"
 license          "All rights reserved"
 description      "Installs/Configures git"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 %w{ ubuntu solaris2 }.each do |os|
   supports os
