c82fc5e51fad9cd763e8324f164b29f9e32915d4
Atalanta Systems Engineering <support@atalanta-systems.com>
2012-02-17 14:58:51 +0000

    Bump version to 2.0.2 Add centos support

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@atalanta-systems.com"
 license          "All rights reserved"
 description      "Installs/Configures git"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.1"
-%w{ ubuntu solaris2 }.each do |os|
+version          "2.0.2"
+%w{ ubuntu solaris2 centos }.each do |os|
   supports os
 end
